6c2f24ad6a523455b58a9b46ee0ae227f9f5e7ed
Badescu Oliver <olibadescu@gmail.com>
2024-10-27 00:11:24 +0300

    improved getDoctorAvailableTime, now returns only available times, not 30 minute time intervals

--- src/main/java/mycode/doctor_appointment_api/app/appointments/repository/AppointmentRepository.java
+++ src/main/java/mycode/doctor_appointment_api/app/appointments/repository/AppointmentRepository.java
@@ -2,7 +2,10 @@ package mycode.doctor_appointment_api.app.appointments.repository;
 
 import mycode.doctor_appointment_api.app.appointments.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +19,8 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Intege
     Optional<List<Appointment>> getAllByPatientId(int id);
 
     Optional<List<Appointment>> findByDoctorIdAndStart(int id, LocalDateTime dateTime);
+
+    @Query("SELECT a FROM Appointment a WHERE a.doctor.id = :doctorId AND DATE(a.start) = :date ORDER BY a.start ASC")
+    Optional<List<Appointment>> findByDoctorIdAndDate(@Param("doctorId") int doctorId, @Param("date") LocalDate date);
 
 }
--- src/main/java/mycode/doctor_appointment_api/app/doctor/dtos/AvailableDoctorTimes.java
+++ src/main/java/mycode/doctor_appointment_api/app/doctor/dtos/AvailableDoctorTimes.java
@@ -5,5 +5,5 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record AvailableDoctorTimes(@NotNull int doctorId,@NotNull List<AvailableTimeSlot> timesList) {
+public record AvailableDoctorTimes(@NotNull int doctorId,@NotNull List<String> timesList) {
 }
